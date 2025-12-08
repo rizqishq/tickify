840124ffcd565d85d522ddd3e01ae525b0009eeb
rizqishq <210781681+rizqishq@users.noreply.github.com>
2025-12-08 17:01:35 +0700

    fix(cors): allow PATCH method for profile picture upload

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
 }));
 
